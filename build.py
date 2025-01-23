7a913dfc1ae1e9c0aad1d8bf3dd92cf7d20a9f94
mylikes <l786112323@gmail.com>
2025-01-23 17:23:19 -0500

    update

--- build.py
+++ build.py
@@ -1,7 +1,17 @@
+from pathlib import Path
 from flask_frozen import Freezer
 from app import app
 
 freezer = Freezer(app)
+
+@freezer.register_generator
+def browse():
+    paths = []
+    for calendar in Path("calendar").iterdir():
+        if calendar.is_file() and calendar.suffix == ".ics":
+            paths.append(calendar.stem)
+    for path in paths:
+        yield {'subpath': path}
 
 if __name__ == '__main__':
     freezer.freeze()
